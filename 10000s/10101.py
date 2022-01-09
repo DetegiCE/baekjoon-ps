6d2a8e878c1007681981ed1ce06ba384395f040f
Ryubin Kim <martinok1103@naver.com>
2022-01-09 09:58:16 +0900

    Create 10101.py

---
+++ 10000s/10101.py
@@ -0,0 +1,7 @@
+a = int(input())
+b = int(input())
+c = int(input())
+if a + b + c == 180 and a == b and b == c: print('Equilateral')
+elif a + b + c == 180 and ((a == b and b != c) or (a == c and b != c) or (b == c and a != b)): print('Isosceles')
+elif a + b + c == 180: print('Scalene')
+else: print('Error')
