98010f3949d44b288f6d07b9eb4d562454a2ba47
Ryubin Kim <martinok1103@naver.com>
2022-01-16 12:08:11 +0900

    Create 14173.py

---
+++ 14000s/14173.py
@@ -0,0 +1,7 @@
+x1, y1, x2, y2 = map(int, input().split())
+x3, y3, x4, y4 = map(int, input().split())
+xst = min(x1, x2, x3, x4)
+xen = max(x1, x2, x3, x4)
+yst = min(y1, y2, y3, y4)
+yen = max(y1, y2, y3, y4)
+print(max(xen-xst, yen-yst)**2)
