c81cf0b0f8f84f0ced36885d1363683483e0eab8
DetegiCE <martinok1103@naver.com>
2022-06-03 17:39:03 +0900

    solve 0603_2

---
+++ 10000s/10531.cpp
@@ -0,0 +1,80 @@
+#include <algorithm>
+#include <cmath>
+#include <complex>
+#include <cstdio>
+#include <vector>
+using namespace std;
+
+#define PI 3.14159265358979323846
+
+typedef long long int ll;
+typedef complex<double> cd;
+
+void fft(vector<cd> &a, bool invert) {
+    int n = a.size();
+    for (int i = 1, j = 0; i < n; i++) {
+        int bit = n >> 1;
+        for (; j >= bit; bit >>= 1) j -= bit;
+        j += bit;
+        if (i < j) swap(a[i], a[j]);
+    }
+    for (int len = 2; len <= n; len <<= 1) {
+        double ang = 2 * PI / len * (invert ? -1 : 1);
+        cd wlen(cos(ang), sin(ang));
+        for (int i = 0; i < n; i += len) {
+            cd w(1);
+            for (int j = 0; j < len / 2; j++) {
+                cd u = a[i + j], v = a[i + j + len / 2] * w;
+                a[i + j] = u + v;
+                a[i + j + len / 2] = u - v;
+                w *= wlen;
+            }
+        }
+    }
+    if (invert) {
+        for (int i = 0; i < n; i++) a[i] /= n;
+    }
+}
+
+void multiply(vector<ll> &a, vector<ll> &b, vector<ll> &ans) {
+    vector<cd> fa(a.begin(), a.end()), fb(b.begin(), b.end());
+
+    int n = 1, m = a.size() + b.size() - 1;
+    while (n < m) n <<= 1;
+
+    fa.resize(n);
+    fb.resize(n);
+
+    fft(fa, false);
+    fft(fb, false);
+
+    for (int i = 0; i < n; i++) fa[i] *= fb[i];
+    fft(fa, true);
+
+    ans.resize(n);
+    for (int i = 0; i < n; i++)
+        ans[i] = int(fa[i].real() + (fa[i].real() > 0 ? 0.5 : -0.5));
+}
+
+int main() {
+    int n;
+    scanf("%d", &n);
+    vector<ll> a(400001);
+    a[0] = 1LL;
+    for (int i = 0; i < n; i++) {
+        int t;
+        scanf("%d", &t);
+        a[t] = 1LL;
+    }
+    vector<ll> ans;
+    multiply(a, a, ans);
+
+    int m, cnt = 0;
+    scanf("%d", &m);
+    while(m--) {
+        int t;
+        scanf("%d", &t);
+        if(ans[t]) cnt++;
+    }
+    printf("%d", cnt);
+}
