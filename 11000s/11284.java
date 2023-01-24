37fd9a10f344e2835e1b780ece8deda50a2886ef
Ryubin Kim <martinok1103@naver.com>
2023-01-24 12:03:31 +0900

    Create 11284.java

---
+++ 11000s/11284.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+	    Scanner sc = new Scanner(System.in);
+	    String chos[] = {"ㄱ", "ㄲ", "ㄴ", "ㄷ", "ㄸ", "ㄹ", "ㅁ", "ㅂ", "ㅃ", "ㅅ", "ㅆ", "ㅇ", "ㅈ", "ㅉ", "ㅊ", "ㅋ", "ㅌ", "ㅍ", "ㅎ"};
+	    String jungs[] = {"ㅏ", "ㅐ", "ㅑ", "ㅒ", "ㅓ", "ㅔ", "ㅕ", "ㅖ", "ㅗ", "ㅘ", "ㅙ", "ㅚ", "ㅛ", "ㅜ", "ㅝ", "ㅞ", "ㅟ", "ㅠ", "ㅡ", "ㅢ", "ㅣ"};
+	    String jongs[] = {"", "ㄱ", "ㄲ", "ㄳ", "ㄴ", "ㄵ", "ㄶ", "ㄷ", "ㄹ", "ㄺ", "ㄻ", "ㄼ", "ㄽ", "ㄾ", "ㄿ", "ㅀ", "ㅁ", "ㅂ", "ㅄ", "ㅅ", "ㅆ", "ㅇ", "ㅈ", "ㅊ", "ㅋ", "ㅌ", "ㅍ", "ㅎ"};
+	    char kor = (sc.next()).charAt(0);
+	    int cho = (kor-0xAC00)/28/21;
+	    int jung = (kor-0xAC00)/28%21;
+	    int jong = (kor-0xAC00)%28;
+	    System.out.println(chos[cho]);
+	    System.out.println(jungs[jung]);
+	    System.out.println(jongs[jong]);
+	}
+}
