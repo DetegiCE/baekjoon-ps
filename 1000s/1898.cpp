10730939bf1901de8491ddd1c9c2ee986e1eb88c
Ryubin Kim <martinok1103@naver.com>
2023-04-19 05:15:44 +0900

    Create 1898.cpp

---
+++ 1000s/1898.cpp
@@ -0,0 +1,29 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int n;
+int used[50001];
+
+int main()
+{
+    cin.tie(0)->sync_with_stdio(0);
+    
+    cin >> n;
+    for(int i=0 ; i<n ; i++) {
+        int t;
+        cin >> t;
+        if(used[t] != 0) {
+            cout << used[t] << '\n';
+            continue;
+        }
+        if(t > 1 && used[t-1] == 0) {
+            cout << t-1 << '\n';
+            used[t-1] = t;
+            used[t] = t-1;
+        }
+        else {
+            cout << t << '\n';
+            used[t] = t;
+        }
+    }
+}
