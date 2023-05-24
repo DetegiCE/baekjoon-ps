69c58c586ffb17618037ee3f396b6a590526eb59
Ryubin Kim <martinok1103@naver.com>
2023-05-24 13:29:56 +0900

    Create 28063.py

---
+++ 28000s/28063.py
@@ -0,0 +1,10 @@
+n = int(input())
+x, y = map(int, input().split())
+if n == 1:
+    print(0)
+elif x in [1,n] and y in [1,n]:
+    print(2)
+elif x in [1,n] or y in [1,n]:
+    print(3)
+else:
+    print(4)
