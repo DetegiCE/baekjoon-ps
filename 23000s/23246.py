6cc88caef1fded60704a98aee5df063c5034ade6
Ryubin Kim <martinok1103@naver.com>
2024-06-18 16:56:34 +0900

    Create 23246.py

---
+++ 23000s/23246.py
@@ -0,0 +1,8 @@
+n = int(input())
+a = []
+for _ in range(n):
+    b, p, q, r = map(int, input().split())
+    a.append([b, p*q*r, p+q+r])
+a.sort(key=lambda x: [x[1], x[2], x[0]])
+for i in range(3):
+    print(a[i][0], end=' ')
